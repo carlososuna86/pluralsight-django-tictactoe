ef78694c9d2f992c2a2965ffcd0178fc2c89d335
Carlos Augusto Osuna <carlososuna86@gmail.com>
2019-09-13 16:06:58 -0400

    Initial Docker configuration

---
+++ web/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+virtual() {
+  if [[ ! -d django-env ]]; then
+    python3 -venv django-env
+  fi
+
+  . django-env/bin/activate
+}
+
+virtual
+
+pip3 install --upgrade pip
+
+if [[ ! -z $@ ]]; then
+  $@
+else
+  python3 manage.py runserver 0.0.0.0:80
+fi
