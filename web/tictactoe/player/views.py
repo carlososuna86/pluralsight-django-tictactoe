90ec62cd45a272b61c0551c9f48fd033eac0ba4f
Carlos Augusto Osuna <carlososuna86@gmail.com>
2019-09-16 17:55:36 -0400

    Demo: Handling Form Submit

--- web/tictactoe/player/views.py
+++ web/tictactoe/player/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import InvitationForm
+from .models import Invitation
 from gameplay.models import Game
 
 @login_required
@@ -14,6 +15,12 @@ def home(request):
 
 @login_required
 def new_invitation(request):
-  form = InvitationForm()
+  if request.method == "POST":
+      invitation = Invitation(from_user=request.user)
+      form = InvitationForm(instance=invitation, data=request.POST)
+      if form.is_valid():
+        form.save()
+        return redirect('player_home')
+  else:
+    form = InvitationForm()
   return render(request, "player/new_invitation_form.html", { 'form': form })
-  
