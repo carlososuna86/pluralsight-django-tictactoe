b05538c17fed5c7b2a43d1686c612985e5dee34e
Carlos Augusto Osuna <carlososuna86@gmail.com>
2019-09-17 14:49:21 -0400

    Improve integration of the Init Script

--- web/init.sh
+++ web/init.sh
@@ -2,19 +2,31 @@
 
 virtual() {
   if [[ ! -d django-env ]]; then
+    echo "> Creating a Virtual Environment"
     python3 -venv django-env
   fi
 
+  echo "> Activating the Virtual Environment"
   . django-env/bin/activate
 }
 
 virtual
 
-pip3 install --upgrade pip
+echo "> Installing packages"
+pip install --upgrade pip
+pip install django
+pip install pylint
 
 if [[ ! -z $@ ]]; then
+  echo "> Running $@"
   $@
 else
+  echo "> Serving the application"
+  cd tictactoe
   python3 manage.py migrate
   python3 manage.py runserver 0.0.0.0:80
+  cd ..
 fi
+
+echo "> Deactivating the Virtual Environment"
+deactivate
